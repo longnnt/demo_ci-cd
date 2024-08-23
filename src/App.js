767b1799e0c2b683b9925a44d38d65b8ac8fbad7
Long Nguyen <47670554+longnnt@users.noreply.github.com>
2024-08-23 22:54:25 +0700

    Using cli codepipeline for workshop1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Update for pipeline
+          Update for pipeline using AWS CLI 
         </p>
         <a
           className="App-link"
